9d565c8d0ea55186d57d63c76d5be0e053952b60
gihyonsd <48745104+gihyonsd@users.noreply.github.com>
2022-07-01 17:28:26 +0900

    타입 변환에 따른 형변환 문제 - 2

--- 인터페이스/CountableTest.java
+++ 인터페이스/CountableTest.java
@@ -1,50 +1,62 @@
 package 도전과제;
 
-interface Countable {
-	void count();
+abstract class Countable {
+	protected String name;
+	protected int num;
+
+	Countable(String name, int num) {
+		this.name = name;
+		this.num = num;
+	}
+
+	abstract void count();
 }
 
+class Bird extends Countable {
 
-class Bird implements Countable {
-	String name;
-	public Bird(String name) {
-		this.name = name;
+	Bird(String name, int num) {
+		super(name, num);
 	}
+
 	@Override
-	public void count() {
-		System.out.println(name+"가 2마리 있다.");
+	void count() {
+		System.out.println(name + "가 " + num + "마리 있다.");
 	}
-	public void fly() {
-		System.out.println("2마리 "+name+"가 날아간다.");
+
+	void fly() {
+		System.out.println(num + "마리 " + name + "가 날아간다.");
 	}
 }
-class Tree implements Countable {
-	String name;
-	public Tree(String name) {
-		this.name = name;
+
+class Tree extends Countable {
+
+	Tree(String name, int num) {
+		super(name, num);
 	}
+
 	@Override
-	public void count() {
-		System.out.println(name+"가 5그루 있다.");
+	void count() {
+		System.out.println(name + "가 " + num + "그루 있다.");
 	}
-	public void ripen() {
-		System.out.println(name+"에 열매가 잘 익었다.");
+
+	void ripen() {
+		System.out.println(num + "그루 " + name + "에 열매가 잘 익었다.");
 	}
-	
+
 }
+
 public class CountableTest {
 	public static void main(String[] args) {
-		Countable[] m = { new Bird("뻐꾸기"), new Bird("독수리"), new Tree("사과나무"), new Tree("밤나무") };
+		Countable[] m = { new Bird("뻐꾸기", 5), new Bird("독수리", 2), new Tree("사과나무", 10), new Tree("밤나무", 7) };
 		
-		for(Countable e : m) {
-			e.count();
+		for(Countable x : m) {
+			x.count();
 		}
-				
-		for(int i = 0; i < m.length; i++) {
-			if(m[i] instanceof Bird) {
-				((Bird)m[i]).fly();
+		for(Countable y : m) {
+			if(y instanceof Bird) {
+				((Bird)y).fly();
 			} else {
-				((Tree)m[i]).ripen();
+				((Tree)y).ripen();
 			}
 		}
 	}
